a2998ca90f02c57c3635a0289fecfe900ed37a60
PianoMan51 <nikolajwold@hotmail.com>
2024-03-21 23:13:01 +0100

    time working

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 let type_test = document.getElementById("type_test");
+let timerElement = document.getElementById("timer");
 let game_active = false;
-let scoreCount = document.getElementById("score");
-let missCount = document.getElementById("miss");
+let startTime;
+let timerInterval;
 
 fillWords();
 
 document.addEventListener("keypress", (event) => {
   if (game_active == false) {
     if (event.keyCode === 32) {
-      game_active = !game_active;
-      checkWords();
+      game_active = true;
+      startTime = new Date();
+      checkWords(startTime);
+      document.querySelector("#press_start").style.opacity = "0.3";
+      document.querySelector("#timer").style.opacity = "1";
+      updateTimer();
     }
   }
 });
@@ -18,7 +23,7 @@ function fillWords() {
   fetch("/words.json")
     .then((response) => response.json())
     .then((words) => {
-      for (let i = 0; i < 5; i++) {
+      for (let i = 0; i < 10; i++) {
         let random = Math.floor(Math.random() * 979);
         for (let j = 0; j < words[random].length; j++) {
           let char = document.createElement("span");
@@ -29,32 +34,41 @@ function fillWords() {
         char.innerHTML = " ";
         type_test.append(char);
       }
+      type_test.children[0].classList.add("active_char");
     });
 }
 
 function checkWords() {
-  let score = 0;
-  let miss = 0;
   let current_char = 0;
-
+  type_test.children[0].classList.add("active_char");
   document.addEventListener("keydown", (event) => {
     if (event.key === type_test.innerText[current_char]) {
       type_test.children[current_char + 1].classList.add("active_char");
       type_test.children[current_char].style.color = "rgba(236, 240, 241, 1)";
       type_test.children[current_char].classList.remove("active_char");
       current_char++;
-      score++;
-      scoreCount.innerHTML = score;
     } else {
       type_test.children[current_char].style.color = "#e74c3c";
-      miss++;
-      missCount.innerHTML = miss;
     }
-    console.log(current_char, type_test.children.length);
     if (current_char == type_test.children.length - 1) {
       type_test.innerHTML = "";
       fillWords();
       current_char = 0;
     }
   });
+}
+
+function updateTimer() {
+  let totalTime = 30;
+  timerInterval = setInterval(() => {
+    let currentTime = new Date();
+    let elapsedTime = (currentTime - startTime) / 1000;
+    let remainingTime = totalTime - elapsedTime;
+    if (remainingTime <= 0) {
+      clearInterval(timerInterval);
+      timerElement.textContent = "0.00";
+      return;
+    }
+    timerElement.textContent = `${remainingTime.toFixed(2)}`;
+  }, 1);
 }
