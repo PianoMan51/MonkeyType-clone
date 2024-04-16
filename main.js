69430763f282f3b93c1234eddf6dde6f49153f67
Nikolaj <nikolajwold@hotmail.com>
2024-04-17 00:29:34 +0200

    words working, and others

--- main.js
+++ main.js
@@ -2,14 +2,15 @@ let typeTest = document.getElementById("type_test");
 let timerElement = document.getElementById("timer");
 let gameActive = false;
 let settings_bar = document.getElementById("settings");
-let scoreboard = JSON.parse(localStorage.getItem("scoreboard")) || [];
 let settingButtons = document.querySelectorAll(".setting_buttons");
 let keyListenerEnabled = true;
+let language_button = document.getElementById("test_language");
 let startTime;
 let wordLength = 5;
 let timerInterval;
 let correctChar;
 let incorrectChar;
+let currentCategory = "time";
 
 let testTime;
 let currentIndex;
@@ -17,15 +18,29 @@ let currentIndex;
 document.addEventListener("keypress", (event) => {
   if (event.keyCode === 32 && keyListenerEnabled) {
     if (!gameActive) {
-      console.log("Start");
       startGame();
     }
+  }
+});
+
+language_button.addEventListener("click", () => {
+  if (language_button.innerText == "english") {
+    language_button.innerHTML =
+      '<i class="fa-solid fa-earth-americas fa-xl"></i>danish';
+  } else {
+    language_button.innerHTML =
+      '<i class="fa-solid fa-earth-americas fa-xl"></i>english';
+  }
+  if (gameActive) {
+    fillWords();
   }
 });
 
 document.addEventListener("keydown", checkWords);
 
 settingButtons.forEach((btn) => {
+  let wordCounts = [25, 50, 75, 100];
+  let times = [5, 15, 30, 60];
   btn.addEventListener("click", function () {
     if (btn.classList[1] == "type") {
       document.querySelectorAll(".type").forEach((b) => {
@@ -33,6 +48,24 @@ settingButtons.forEach((btn) => {
       });
       if (btn.classList[2] !== "active") {
         btn.classList.add("active");
+        currentCategory = btn.innerText;
+        if (btn.innerText == "words") {
+          document
+            .querySelectorAll(".setting_buttons.time")
+            .forEach((val, index) => {
+              val.querySelector("span").innerHTML = wordCounts[index];
+            });
+        }
+        if (btn.innerText == "time") {
+          document
+            .querySelectorAll(".setting_buttons.time")
+            .forEach((val, index) => {
+              val.querySelector("span").innerHTML = times[index];
+            });
+        }
+        if (gameActive) {
+          fillWords();
+        }
       } else {
         btn.classList.remove("active");
       }
@@ -43,9 +76,10 @@ settingButtons.forEach((btn) => {
       });
       if (btn.classList[2] !== "active") {
         btn.classList.add("active");
-        let newTime = btn.childNodes[1].getAttribute("value");
+        let newTime = btn.childNodes[1].innerHTML;
         if (gameActive) {
           timerElement.innerHTML = newTime;
+          fillWords();
         }
         testTime = newTime;
       } else {
@@ -68,28 +102,42 @@ function startGame() {
 }
 
 function fillWords() {
-  testTime = document
-    .querySelector(".setting_buttons.time.active")
-    .childNodes[1].getAttribute("value");
+  let words = `/${language_button.innerText}.json`;
+  testTime = document.querySelector(".setting_buttons.time.active")
+    .childNodes[1].innerHTML;
   timerElement.textContent = testTime;
-  fetch("/words.json")
+  fetch(words)
     .then((response) => response.json())
     .then((words) => {
-      let wordCount = 0;
       typeTest.innerHTML = "";
-      for (let i = 0; i < 30; i++) {
-        let random = Math.floor(Math.random() * words.length);
-        for (let j = 0; j < words[random].length; j++) {
+      if (currentCategory == "time") {
+        for (let i = 0; i < 30; i++) {
+          let random = Math.floor(Math.random() * words.length);
+          for (let j = 0; j < words[random].length; j++) {
+            let char = document.createElement("span");
+            char.innerHTML = words[random][j];
+            typeTest.append(char);
+          }
           let char = document.createElement("span");
-          char.innerHTML = words[random][j];
+          char.innerHTML = " ";
           typeTest.append(char);
-          wordCount++;
         }
-        let char = document.createElement("span");
-        char.innerHTML = " ";
-        typeTest.append(char);
+        typeTest.children[0].classList.add("active_char");
       }
-      typeTest.children[0].classList.add("active_char");
+      if (currentCategory == "words") {
+        for (let i = 0; i < testTime; i++) {
+          let random = Math.floor(Math.random() * words.length);
+          for (let j = 0; j < words[random].length; j++) {
+            let char = document.createElement("span");
+            char.innerHTML = words[random][j];
+            typeTest.append(char);
+          }
+          let char = document.createElement("span");
+          char.innerHTML = " ";
+          typeTest.append(char);
+        }
+        typeTest.children[0].classList.add("active_char");
+      }
     });
 }
 
@@ -101,6 +149,7 @@ function checkWords(event) {
 
   if (gameActive) {
     settings_bar.style.opacity = "0";
+    language_button.style.opacity = "0";
 
     if (!startTime && event.key == typeTest.innerText[0]) {
       startTime = new Date();
@@ -132,9 +181,11 @@ function checkWords(event) {
         }
       }
       if (nextElement == null) {
-        typeTest.innerHTML = "";
-        fillWords();
-        currentIndex = 0;
+        if (currentCategory == "time") {
+          typeTest.innerHTML = "";
+          fillWords();
+          currentIndex = 0;
+        }
       }
     }
   }
@@ -143,54 +194,85 @@ function checkWords(event) {
 function updateTimer(startTime) {
   let timerInterval;
 
-  timerInterval = setInterval(() => {
-    let currentTime = new Date();
-    let elapsedTime = (currentTime - startTime) / 1000;
-    let remainingTime = testTime - elapsedTime;
+  if (currentCategory == "time") {
+    timerInterval = setInterval(() => {
+      let currentTime = new Date();
+      let elapsedTime = (currentTime - startTime) / 1000;
+      let remainingTime = testTime - elapsedTime;
 
-    if (remainingTime <= 0) {
-      clearInterval(timerInterval);
-      let score = ((correctChar / wordLength / testTime) * 60).toFixed(0);
-      let accuracy = (correctChar / (incorrectChar + correctChar)) * 100;
-      typeTest.innerHTML = score + "wpm";
-      document.getElementById("wpm").innerHTML = score;
-      let now = new Date();
+      if (remainingTime <= 0) {
+        clearInterval(timerInterval);
 
-      let hours = now.getHours();
-      let minutes = now.getMinutes();
+        gameActive = false;
+        keyListenerEnabled = false;
 
-      let game_score = {
-        time: `${hours}:${minutes}`,
-        score: score,
-        testTime: testTime,
-      };
+        gameDone();
 
-      scoreboard.push(game_score);
-      localStorage.setItem("scoreboard", JSON.stringify(scoreboard));
+        setTimeout(() => {
+          keyListenerEnabled = true;
+          timerElement.textContent = "Press space for restart";
+        }, 1200);
 
-      gameActive = false;
-      settings_bar.style.opacity = "1";
+        return;
+      }
+      timerElement.textContent = remainingTime.toFixed(2);
+    }, 1);
+  }
 
-      keyListenerEnabled = false;
-      console.log("Wait two seconds");
-      document.getElementById(
-        "testType"
-      ).innerHTML = `time ${testTime}<br> english`;
-      document.getElementById("acc").innerHTML = `${Math.trunc(accuracy)}%`;
-      document.getElementById("game_result").style.display = "flex";
-      document.getElementById(
-        "stat_characters"
-      ).innerHTML = `${correctChar}/${incorrectChar}`;
-      document.getElementById("stat_time").innerHTML = `${testTime}s`;
-      setTimeout(() => {
-        keyListenerEnabled = true;
-        console.log("Ready");
-        timerElement.textContent = "Press space for restart";
-      }, 1000);
+  if (currentCategory == "words") {
+    timerInterval = setInterval(() => {
+      let currentTime = new Date();
 
-      return;
-    }
+      let elapsedTime = (currentTime - startTime) / 1000;
+      timerElement.textContent = elapsedTime.toFixed(2);
 
-    timerElement.textContent = remainingTime.toFixed(2);
-  }, 1);
+      if (currentIndex == typeTest.innerText.length) {
+        clearInterval(timerInterval);
+
+        gameActive = false;
+        keyListenerEnabled = false;
+        gameDone();
+
+        setTimeout(() => {
+          keyListenerEnabled = true;
+          timerElement.textContent = "Press space for restart";
+        }, 1200);
+      }
+    }, 1);
+  }
+}
+
+function gameDone() {
+  let accElement = document.getElementById("acc");
+  let testTypeElement = document.getElementById("testType");
+  let wpmElement = document.getElementById("wpm");
+  let statCharactersElement = document.getElementById("stat_characters");
+  let statTimeElement = document.getElementById("stat_time");
+  let gameResultElement = document.getElementById("game_result");
+  let wpm;
+
+  if (currentCategory == "time") {
+    wpm = ((correctChar / wordLength / testTime) * 60).toFixed(0);
+  }
+  if (currentCategory == "words") {
+    wpm = ((testTime / timerElement.textContent) * 60).toFixed(0);
+  }
+
+  let accuracy = ((correctChar / (incorrectChar + correctChar)) * 100).toFixed(
+    0
+  );
+
+  typeTest.innerHTML = `${wpm}wpm`;
+  wpmElement.innerHTML = wpm;
+  testTypeElement.innerHTML = `${currentCategory} ${testTime}s<br>${language_button.innerText}`;
+  accElement.innerHTML = `${accuracy}%`;
+  statCharactersElement.innerHTML = `${correctChar}/${incorrectChar}`;
+  statTimeElement.innerHTML =
+    currentCategory === "time"
+      ? `${testTime}s`
+      : `${timerElement.textContent}s`;
+
+  settings_bar.style.opacity = "1";
+  language_button.style.opacity = "1";
+  gameResultElement.style.display = "flex";
 }
